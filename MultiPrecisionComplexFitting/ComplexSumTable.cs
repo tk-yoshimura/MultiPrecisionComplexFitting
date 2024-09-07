8a98e7b5921fa630ca94876ac7f07ec223bf2926
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-08 00:53:49 +0900

    fix: sum_table

--- MultiPrecisionComplexFitting/ComplexSumTable.cs
+++ MultiPrecisionComplexFitting/ComplexSumTable.cs
@@ -95,6 +95,10 @@ namespace MultiPrecisionComplexFitting {
         public Vector<N>? W {
             get => w;
             set {
+                if (ReferenceEquals(this.w, w)) {
+                    return;
+                }
+
                 if (value is not null && xs[0].Dim != value.Dim) {
                     throw new ArgumentException("invalid size", nameof(W));
                 }
