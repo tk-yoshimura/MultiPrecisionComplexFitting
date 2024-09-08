d3cd690663608c926e38e85259343cb7758d7f8e
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-08 15:27:38 +0900

    fix: sandbox

--- MultiPrecisionComplexFittingSandbox/Program.cs
+++ MultiPrecisionComplexFittingSandbox/Program.cs
@@ -1,16 +1,64 @@
 ﻿using MultiPrecision;
 using MultiPrecisionComplex;
+using MultiPrecisionComplexAlgebra;
+using MultiPrecisionComplexFitting;
+using MultiPrecisionCurveFitting;
 
 namespace MultiPrecisionComplexFittingSandbox {
     internal class Program {
         static void Main() {
-            for (int x = 256; x >= 8; x--) {
-                Complex<Pow2.N16> y = ComplexGammaN16.Gamma((x + 0.5, 1));
+            ComplexVector<Pow2.N16> xs = ComplexMatrix<Pow2.N16>.Flatten(ComplexMatrix<Pow2.N16>.Grid((-32, 32), (-32, 32)) / 64);
+            ComplexVector<Pow2.N16> ys = (x => ComplexGammaN16.Gamma(x + (1, 1.5)), xs);
 
-                Console.WriteLine($"{(x + 0.5, 1)},{y}");
+            ComplexSumTable<Pow2.N64> table = new(xs.Convert<Pow2.N64>(), ys.Convert<Pow2.N64>());
 
-                if (Complex<Pow2.N16>.IsNaN(y)) {
-                    break;
+            bool finished = false; 
+            for (int coefs = 5; coefs <= 256 && !finished; coefs++) {
+                foreach ((int m, int n) in CurveFittingUtils.EnumeratePadeDegree(coefs, 2)) {
+                    ComplexPadeFitter<Pow2.N64> pade = new(table, m, n);
+
+                    ComplexVector<Pow2.N64> param = pade.Fit();
+                    ComplexVector<Pow2.N64> errs = pade.Error(param);
+
+                    ComplexVector<Pow2.N16> vs = pade.Regress(xs.Convert<Pow2.N64>(), param).Convert<Pow2.N16>();
+
+                    MultiPrecision<Pow2.N16> max_rateerr = ComplexCurveFittingUtils.MaxRelativeError(ys, vs);
+
+                    Console.WriteLine($"m={m},n={n}");
+                    Console.WriteLine($"{max_rateerr:e20}");
+
+                    if (max_rateerr > "1e-12") {
+                        coefs += 4;
+                        break;
+                    }
+
+                    if (max_rateerr < "1e-20") {
+                        break;
+                    }
+
+                    if (max_rateerr < "1e-16") {
+                        finished = true;
+
+                        using (StreamWriter sw = new("../../../../results_disused/complex_gamma_precision16.csv")) {
+                            sw.WriteLine($"x=[{xs[0]},{xs[^1]}]");
+                            sw.WriteLine($"samples={xs.Dim}");
+                            sw.WriteLine($"m={m},n={n}");
+                            sw.WriteLine("numer");
+                            foreach (var (_, val) in param[..m]) {
+                                sw.WriteLine($"{val:e38}");
+                            }
+                            sw.WriteLine("denom");
+                            foreach (var (_, val) in param[m..]) {
+                                sw.WriteLine($"{val:e38}");
+                            }
+
+                            sw.WriteLine("relative err");
+                            sw.WriteLine($"{max_rateerr:e20}");
+                            sw.Flush();
+                        }
+
+                        break;
+                    }
                 }
             }
 
